adddcc35efeadbe05657ce512d6070a86ec9f716
Kuroshi117 <phoebehana4@gmail.com>
2018-09-20 17:12:28 -0500

    Fixed SortbyDoubleUsingQuickSort(), PartitionByDouble(), SortbyIDUsingQuickSort() and PartitionByID(). SortbyIDUsingQuickSort() runs into worst case scenario, so pivot if different (see comments). Removed Test List<> in Main(), added Stopwatch from System.Diagnostics to time execution of Quicksort. Still need to figure out how to quicksort GUIDs.

--- AlgSortConsole/Program.cs
+++ AlgSortConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Diagnostics;
 
 namespace AlgSortConsole
 {
@@ -11,12 +12,19 @@ namespace AlgSortConsole
     {
         //Guid->Decimal
         public static List<Tuple<int, Guid, double>> Tupperware = new List<Tuple<int, Guid, double>>();
+        
         static void Main(string[] args)
         {
-            //ReadText(@"C:\workspace\FakeData.csv");
-            List<int> Test = new List<int>(new int[] { 8, 0, 3, 4, 5, 6, 4 });
-            Partition(Test, 0, Test.Count-1);
-
+            ReadText(@"C:\workspace\FakeData.csv");
+            Stopwatch sw = Stopwatch.StartNew();
+            SortByDoubleUsingQuickSort(Tupperware, 0, 1000000);
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds+" ms");
+            for (int i = 0; i < Tupperware.Count; i++)
+            {
+                Console.Write(i.ToString()+": ");
+                Console.WriteLine(Tupperware[i].ToString());
+            }
             Console.ReadKey();
         }
 
@@ -24,59 +32,98 @@ namespace AlgSortConsole
         {
             string line;
             string[] words = new string[3];
-            using (StreamReader sr=new StreamReader(path))
+            using (StreamReader sr = new StreamReader(path))
             {
-                while((line = sr.ReadLine()) != null)
+                while ((line = sr.ReadLine()) != null)
                 {
                     words = line.Split(',');
-                    Tupperware.Add(Tuple.Create(Convert.ToInt32(words[0]), Guid.Parse(words[1]) , Convert.ToDouble(words[2])));
-                    
+                    Tupperware.Add(Tuple.Create(Convert.ToInt32(words[0]), Guid.Parse(words[1]), Convert.ToDouble(words[2])));
+
                 }
-               
+
             }
         }
 
-
-        public static void QuickSort(List<int> Sort, int low, int high)
+        //int versions
+        //Worse case scenario-if pivot = largest/smallest value, each successive list is n-1 size
+        //So, for now, pivot is (high-low)/2+low
+        public static void SortByIDUsingQuickSort(List<Tuple<int, Guid, double>> Sort, int low, int high)
         {
-            List<int> SortList = new List<int>();
-            for (int i = 0; i < Sort.Count; i++)
+            if (low < high)
             {
-                SortList.Add(Sort[i].Item1);
+                int pi = PartitionByID(Sort, low, high);
+                SortByIDUsingQuickSort(Sort, low, pi - 1);
+                SortByIDUsingQuickSort(Sort, pi + 1, high);
             }
-
-            
+      
         }
-        public static int Partition(List<int> Sort, int low, int high)
+        public static int PartitionByID(List<Tuple<int, Guid, double>> Sort, int low, int high)
         {
-            int pivot = Sort[high];//pivot = last value
-            int LIndex = (low - 1);//Lower index initialized at low-1 so that when LIndex is needed, it will start at index 0 
-            for (int RIndex = low; RIndex < (high - 1); RIndex++)//Higher Index that starts at low and goes to the value before pivot
+            int pivot = Sort[(high-low)/2+low].Item1;
+            int LIndex = (low - 1);
+            for (int RIndex = low; RIndex < (high - 1); RIndex++)
             {
-                if (Sort[RIndex] <= Sort[pivot])//if RIndex value is smaller/equal to pivot value
+                if (Sort[RIndex].Item1 <= pivot)
                 {
-                    LIndex++;//increment LIndex
-                    int temp1 = Sort[LIndex];
+                    LIndex++;
+                    Tuple<int, Guid, double> temp1 = Sort[LIndex];
                     Sort[LIndex] = Sort[RIndex];
                     Sort[RIndex] = temp1;
-                    //what it does is that when Partition() finds a value less than pivot, it swaps it with the value after LIndex by having LIndex inrecrement to it and then swapping
-                    //In doing so, it pushes the lesser value up the list, and should it push a high value up the list, it will eventually swap it as LIndex with a higher valie RIndex down the line
                 }
-                int temp2 = Sort[LIndex+1];
-                Sort[LIndex+1] = Sort[high];
+                Tuple<int, Guid, double> temp2 = Sort[LIndex + 1];
+                Sort[LIndex + 1] = Sort[high];
                 Sort[high] = temp2;
-                //Once done sorting through everything else, LIndex will eventually be at the index before where pivot should be, so LIndex+1 index has to be swapped with pivot index
-                //Also have to swap in Partition because scopes
             }
-
-
-            for (int i = 0; i < Sort.Count; i++)
-            {
-                Console.WriteLine(Sort[i].ToString());
-            }
-
             return LIndex + 1;
         }
+
+        //double versions
+        public static void SortByDoubleUsingQuickSort(List<Tuple<int, Guid, double>> Sort, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = PartitionByDouble(Sort, low, high);
+                SortByDoubleUsingQuickSort(Sort, low, pi - 1);
+                SortByDoubleUsingQuickSort(Sort, pi + 1, high);
+            }
+            
+        }
+        public static int PartitionByDouble(List<Tuple<int, Guid, double>> Sort, int low, int high)
+        {
+            double pivot = Sort[high].Item3;
+            int LIndex = (low - 1);
+            for (int RIndex = low; RIndex < (high - 1); RIndex++)
+            {
+                if (Sort[RIndex].Item3 <= pivot)
+                {
+                    LIndex++;
+                    Tuple<int, Guid, double> temp1 = Sort[LIndex];
+                    Sort[LIndex] = Sort[RIndex];
+                    Sort[RIndex] = temp1;
+                }
+                Tuple<int, Guid, double> temp2 = Sort[LIndex + 1];
+                Sort[LIndex + 1] = Sort[high];
+                Sort[high] = temp2;
+            }
+            return LIndex + 1;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
