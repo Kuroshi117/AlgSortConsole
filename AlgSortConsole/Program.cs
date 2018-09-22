90c77a31c537a1f7370a82e73690a992db40094f
Phoebe Huang <phoebehana4@gmail.com>
2018-09-22 17:34:05 -0500

    Figured out how to sort Guids using Guid.CompareTo(). Created SortByGUIDUsingQuickSort(), PartitionByGUID(), and SortByGUIDUsingCombSort().

--- AlgSortConsole/Program.cs
+++ AlgSortConsole/Program.cs
@@ -10,14 +10,14 @@ namespace AlgSortConsole
 {
     class Program
     {
-        //Guid->Decimal
         public static List<Tuple<int, Guid, double>> Tupperware = new List<Tuple<int, Guid, double>>();
         
         static void Main(string[] args)
         {
             ReadText(@"C:\Users\Potato\Downloads\FakeData.csv");
             Stopwatch sw = Stopwatch.StartNew();
-            SortByDoubleUsingCombSort(Tupperware);
+            //remember to use Tupperware.Count-1 when using Quicksort
+            SortByGUIDUsingQuickSort(Tupperware, 0, Tupperware.Count-1);
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds+" ms");
             for (int i = 0; i < Tupperware.Count; i++)
@@ -77,7 +77,36 @@ namespace AlgSortConsole
             }
             return LIndex + 1;
         }
+        //GUID versions
+        public static void SortByGUIDUsingQuickSort(List<Tuple<int, Guid, double>> Sort, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = PartitionByGUID(Sort, low, high);
+                SortByGUIDUsingQuickSort(Sort, low, pi - 1);
+                SortByGUIDUsingQuickSort(Sort, pi + 1, high);
+            }
 
+        }
+        public static int PartitionByGUID(List<Tuple<int, Guid, double>> Sort, int low, int high)
+        {
+            Guid pivot = Sort[(high - low) / 2 + low].Item2;
+            int LIndex = (low - 1);
+            for (int RIndex = low; RIndex < (high - 1); RIndex++)
+            {
+                if (Sort[RIndex].Item2.CompareTo(pivot) <= 0)
+                {
+                    LIndex++;
+                    Tuple<int, Guid, double> temp1 = Sort[LIndex];
+                    Sort[LIndex] = Sort[RIndex];
+                    Sort[RIndex] = temp1;
+                }
+                Tuple<int, Guid, double> temp2 = Sort[LIndex + 1];
+                Sort[LIndex + 1] = Sort[high];
+                Sort[high] = temp2;
+            }
+            return LIndex + 1;
+        }
         //double versions
         public static void SortByDoubleUsingQuickSort(List<Tuple<int, Guid, double>> Sort, int low, int high)
         {
@@ -91,7 +120,7 @@ namespace AlgSortConsole
         }
         public static int PartitionByDouble(List<Tuple<int, Guid, double>> Sort, int low, int high)
         {
-            double pivot = Sort[high].Item3;
+            double pivot = Sort[(high - low) / 2 + low].Item3;
             int LIndex = (low - 1);
             for (int RIndex = low; RIndex < (high - 1); RIndex++)
             {
@@ -141,7 +170,35 @@ namespace AlgSortConsole
 
             }
         }
+        //GUID version
+        public static void SortByGUIDUsingCombSort(List<Tuple<int,Guid, double>> Sort)
+        {
+            int n = Sort.Count;
+            int gap = n;
+            bool swapped = true;//true to gurantee that the loop runs
+            while (gap != 1 || swapped == true)
+            {
+                //next gap
+                gap = (gap * 10) / 13;
+                if (gap < 1)
+                {
+                    gap = 1;
+                }
 
+                swapped = false;
+                for (int i = 0; i < n - gap; i++)
+                {
+                    if (Sort[i].Item2.CompareTo(Sort[i + gap].Item2) > 0 )
+                    {
+                        Tuple<int, Guid, double> temp = Sort[i];
+                        Sort[i] = Sort[i + gap];
+                        Sort[i + gap] = temp;
+                        swapped = true;
+                    }
+                }
+
+            }
+        }
         //double version
         public static void SortByDoubleUsingCombSort(List<Tuple<int, Guid, double>> Sort)
         {
@@ -173,10 +230,6 @@ namespace AlgSortConsole
         }
 
         //geeksforgeeks.org for reference
-
-
-
-
 
 
 
